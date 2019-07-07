80ddd34b4e5ab27470b4e387beb4808ed6302d53
Defaf <defaf_kamfar@hotmail.com>
2019-07-07 17:22:26 +0300

    Creating Map API using Current location

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  // To get latitude and longitude of current user location 
+  state = { 
+    userLocation: { 
+            lat: 32, 
+            lng: 32 
+          }, 
+    loading: true 
+    };
+
+  componentDidMount(props) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        //Change the userLocation value 
+        this.setState({
+          userLocation: { lat: latitude, lng: longitude },
+          loading: false
+        });
+      },
+      () => {
+        //Change the loading value 
+        this.setState({ loading: false });
+      }
+    );
+  }
+
+  render(){
+    const { loading, userLocation } = this.state;
+    const { google } = this.props;
+
+    if (loading) {
+      return null;
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+            <Map google={google} initialCenter={userLocation} zoom={14} >
+                { console.log(this.state.userLocation.lat) }
+                { console.log(this.state.userLocation.lng) }
+                <Marker onClick={this.onMarkerClick}  name={'Current location'} />
+            </Map>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+// export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDIAICVZcHIUeFjNMsHhoNoYcKHuYWdCuY")
+})(App)
