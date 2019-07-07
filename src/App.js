4cb607bcff3788912e0f40a2ceecd60921637d8b
Salman778 <alk-333@hotmil.com>
2019-07-07 11:21:32 +0300

    nthing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import {Route ,Link } from 'react-router-dom';
 import Details from './Details';
 import Lost from './Lost';
 import './App.css';
-
+import Lost from './Lost'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Lost />
       </header>
     </div>
   );
--- src/Lost.js
+++ src/Lost.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import {Route ,Link } from 'react-router-dom'
 import './Lost.css';
 
 class Lost extends Component {
