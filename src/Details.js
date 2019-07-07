8dbe5c47c37edc39acf8f54ad3b42067f5c1582c
somayahbahkeem <iove36@hotmail.com>
2019-07-07 10:45:39 +0300

    creat api file

---
+++ src/Details.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import {Route ,Link } from 'react-router-dom'
+import './Details.css';
+
+
+class Details extends Component {
+    state = {
+      
+    };
+    render() {
+      return (
+        <div className="Details">
+        </div>
+      );
+    }
+  }
+
+
+
+export default Details;
---
+++ src/Lost.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import {Route ,Link } from 'react-router-dom'
+import './Lost.css';
+
+class Lost extends Component {
+    state = {
+      
+    };
+    render() {
+      return (
+        <div className="Lost">
+        </div>
+      );
+    }
+  }
+
+
+export default Lost;
