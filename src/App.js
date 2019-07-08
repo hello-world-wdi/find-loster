c0bbc45aeffe995ccd7a4f14f8733fa76d9f729e
Salman778 <alk-333@hotmil.com>
2019-07-08 17:08:46 +0300

    modfied some lines

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import MapContainer from './MapContainer'
 import Nav from './Nav';
 class App extends React.Component {
   render(){
+    
     return (
       <div className="App">
         <Nav />
-        <Route ecact path='/' component={MapContainer} />
+        <Route exact path='/' component={MapContainer} />
         <Route exact path='/lost/' component={Lost} />
-        <Route exact path='/details/:id' component={Details} />
+        <Route exact path='/lost/details/:id' component={Details} />
       </div>
     );
   }
