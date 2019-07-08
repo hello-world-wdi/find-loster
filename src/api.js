933437fdb8916526573e062a6adf3984a2442624
Salman778 <alk-333@hotmil.com>
2019-07-08 17:14:10 +0300

    add firebase api and google api for location

--- src/api.js
+++ src/api.js
@@ -1,8 +1,39 @@
 import axios from 'axios';
+import firebase from 'firebase';
 
-export const losses = () => {
+const losses = () => {
   return axios({
     method: 'get',
     url: 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=namus-missings&facet=cityoflastcontact&facet=countydisplaynameoflastcontact&facet=raceethnicity&facet=statedisplaynameoflastcontact&facet=gender'
   })
 }
+
+ const config = {
+  apiKey: "AIzaSyCTbBcJZLB5hmPm3LEpQXMkrbc60eOgQYc",
+    authDomain: "learningtoreact.firebaseapp.com",
+    databaseURL: "https://learningtoreact.firebaseio.com",
+    projectId: "learningtoreact",
+    storageBucket: "",
+    messagingSenderId: "203802469519",
+    appId: "1:203802469519:web:2ddcf658d709d916",
+  "rules": {
+    ".read": "auth != null",
+    ".write": "auth != null"
+  }
+  
+};
+firebase.initializeApp(config);
+
+const getCity = () => {
+  return axios({
+    method: 'get',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=21.5678976,39.1856128&key=AIzaSyDIAICVZcHIUeFjNMsHhoNoYcKHuYWdCuY'
+  })
+}
+const api = {
+  losses : losses,
+  firebase : firebase,
+  getCity: getCity
+}
+
+export default api;
