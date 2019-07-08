d94cf19d7f70aca3d301df99950c0e3147df3233
Salman778 <alk-333@hotmil.com>
2019-07-08 11:12:23 +0300

    add Nav and add MapContainer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Lost from './Lost';
 import { Route } from 'react-router-dom';
-import Details from './Details'
-function App() {
-  return (
-    <div className="App">
-      <Route exact path='/' component={Lost} />
-      <Route path='/details/:id' component={Details} />
-    </div>
-  );
+import Details from './Details';
+import MapContainer from './MapContainer'
+import Nav from './Nav';
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <Nav />
+        <Route ecact path='/' component={MapContainer} />
+        <Route exact path='/lost/' component={Lost} />
+        <Route exact path='/details/:id' component={Details} />
+      </div>
+    );
+  }
 }
 
 export default App;
