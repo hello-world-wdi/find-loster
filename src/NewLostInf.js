3de37d121541f905af2aff93c1dfbdc4802850cd
Salman Altuwayjiri <37655980+Salman778@users.noreply.github.com>
2019-07-10 17:45:44 +0300

    modfied some lines

--- src/NewLostInf.js
+++ src/NewLostInf.js
@@ -39,35 +39,49 @@ class NewLostInf extends React.Component {
   }
   render() {
     return (
-      <div className='NewLostInf'>
-        <h1>Information</h1>
-        <form onSubmit={this.handelSubmit}>
-          <label><h3>Your Information</h3></label>
-          <label>Name: </label>
-          <input name='uname' type='text' value={this.state.formData.name} onChange={this.handelChange}/>
-          <label>Phone: </label>
-          <input name='uphone'type='text' value={this.state.formData.phone} onChange={this.handelChange}/>
-          <hr/>
-          <label><h3>Lose Information</h3></label>
+      <main className='NewLostInf'>
+                <aside className="section1">
+      
+      <form onSubmit={this.handelSubmit}>
+        <div id="first">
+       <h3>Lose Information</h3>
+        <label>Name </label>
+        <input name='name' type='text' className="BoxForm"  value={this.state.formData.lose.name} onChange={this.handelChange}/><br/><br/>
+        <label>City </label>
+        <input name='city'type='text' placeholder=" Name" className="BoxForm"  value={this.state.formData.lose.city} onChange={this.handelChange}/><br/><br/>
+        <label>Age </label>
+        <input name='age' className="BoxForm"  type='number' value={this.state.formData.lose.age} onChange={this.handelChange}/><br/><br/>
+        <label>Date </label>
+        <input name='date' className="BoxForm"  type='date' value={this.state.formData.lose.date} onChange={this.handelChange}/><br/><br/>
+        <label className="genderSpace">Gender: </label>
+        <input name="gender" value='male' type="radio" value={this.state.formData.lose.gender} onChange={this.handelChange}/>Male
+        <input name="gender" value='female' type="radio" value={this.state.formData.lose.gender} onChange={this.handelChange}/>Female
 
-          <label>Name: </label>
-          <input name='name' type='text' value={this.state.formData.lose.name} onChange={this.handelChange}/>
-          <label>Gender: </label>
-          <select name="gender" value={this.state.formData.lose.gender} onChange={this.handelChange}>
-          <option value=''></option>
-            <option value='male'>Male</option>
-            <option value='female'>Female</option>
-          </select>
-          <label>City: </label>
-          <input name='city'type='text' value={this.state.formData.lose.city} onChange={this.handelChange}/>
-          <label>Age: </label>
-          <input name='age' type='number' value={this.state.formData.lose.age} onChange={this.handelChange}/>
-          <label>Date: </label>
-          <input name='date' type='date' value={this.state.formData.lose.date} onChange={this.handelChange}/>
-          <br/>
+        </div>
+      </form>
+      </aside>
+
+      <hr class="hr"/>
+
+         <aside className="section2">
+        <form onSubmit={this.handelSubmit}>
+          <div id="first">
+          <h3>Your Information</h3>
+          <label>Name</label>
+          <input   className="BoxForm" name='uname' type='text' value={this.state.formData.name} onChange={this.handelChange}/><br/><br/>
+          <label>Phone</label>
+          <input className="BoxForm" name='uphone'type='text' value={this.state.formData.phone} onChange={this.handelChange}/><br/><br/>
+
+          <div className="submit">
           <input type='Submit' value='Save' />
+          </div>
+
+          </div>
         </form>
-      </div>
+        </aside>
+        
+
+      </main>
     );
   }
 }
